54d8f3d2598cb7568773d717c8b211526634bbd1
ROYS-WIN-PC <royda2@mta.ac.il>
2025-08-30 13:48:09 +0300

    project init

---
+++ src/index.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+
+console.log('Spotify ↔ YouTube Playlist Sync — dry run');
+console.log('Config file will be read from ./config.json');
+console.log('Env will be read from ./.env');
